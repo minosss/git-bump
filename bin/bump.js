c1645f5f0c7255ecb0543963b10aeb205dc0206c
minosss <minoscc+dev@gmail.com>
2022-07-17 16:55:32 +0800

    fix: module exports

--- bin/bump.js
+++ bin/bump.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 // eslint-disable-next-line unicorn/prefer-module
-require('../dist/index.js')(process.argv.slice(2));
+require('../dist/index.js').default(process.argv.slice(2));
